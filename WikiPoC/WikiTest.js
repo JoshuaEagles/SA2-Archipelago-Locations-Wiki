13e9abdb8508c4b1f240067e2ea76e85b6621b1b
JoshuaEagles <joshuaeagles1@gmail.com>
2025-04-19 14:16:41 -0400

    refactor and reduce code duplication

--- WikiPoC/WikiTest.js
+++ WikiPoC/WikiTest.js
@@ -1,33 +1,53 @@
-// Setup pipe toggling
-let pipeLocations = document.querySelectorAll(".location[data-type=pipe]");
-let hiddenLocations = document.querySelectorAll(".location[data-type=hidden]");
-let allLocations = document.querySelectorAll(".location");
+const locationTypeNames = ["pipe", "hidden"];
 
-let pipeToggleCheckbox = document.querySelector("#pipe-toggle");
-pipeToggleCheckbox.addEventListener("change", function () {
-	let checked = this.checked;
-	pipeLocations.forEach(function (pipeLocation) {
-		pipeLocation.classList.toggle("disabled", !checked);
+const locationWrapper = document.querySelector("#location-wrapper");
+
+setupLocationTypeToggling();
+setupChronologicalToggling();
+
+function setupLocationTypeToggling() {
+	locationTypeNames.forEach(function (locationTypeName) {
+		setupLocationTypeFilterButtonListener(locationTypeName);
 	});
-});
 
-let hiddenToggleCheckbox = document.querySelector("#hidden-toggle");
-hiddenToggleCheckbox.addEventListener("change", function () {
-	let checked = this.checked;
-	hiddenLocations.forEach(function (hiddenLocation) {
-		hiddenLocation.classList.toggle("disabled", !checked);
+	function setupLocationTypeFilterButtonListener(locationTypeName) {
+		let locationTypeCheckbox = document.querySelector(
+			`#${locationTypeName}-toggle`,
+		);
+
+		let locationElementsOfType = document.querySelectorAll(
+			`.location[data-type=${locationTypeName}]`,
+		);
+
+		locationTypeCheckbox.addEventListener("change", function () {
+			let checked = this.checked;
+			locationElementsOfType.forEach(function (location) {
+				location.classList.toggle("disabled", !checked);
+			});
+		});
+	}
+}
+
+function setupChronologicalToggling() {
+	let toggleChronologicalButton = document.querySelector(
+		"#chronological-toggle",
+	);
+	let chronologicalState = false;
+	toggleChronologicalButton.addEventListener("click", function () {
+		chronologicalState = !chronologicalState;
+
+		if (chronologicalState === true) {
+			sortLocationsChronologically();
+		} else {
+			sortLocationsByType();
+		}
 	});
-});
 
-let chronologicalState = false;
-let toggleChronologicalButton = document.querySelector("#chronological-toggle");
-let typeOrdering = ["pipe", "hidden"];
-let locationMap = new Map();
-toggleChronologicalButton.addEventListener("click", function () {
-	chronologicalState = !chronologicalState;
+	function sortLocationsChronologically() {
+		const allLocations = document.querySelectorAll(".location");
 
-	if (chronologicalState == true) {
-		// Chronological enabled
+		let locationMap = new Map();
+
 		allLocations.forEach(function (location) {
 			locationMap.set(location.id, location);
 		});
@@ -45,13 +65,16 @@ toggleChronologicalButton.addEventListener("click", function () {
 			nextLocationId = nextLocation.dataset.chronologicalNextLocation;
 		}
 
-		let locationWrapper = document.querySelector("#location-wrapper");
 		locationWrapper.innerHtml = null;
 		locationWrapper.appendChild(sortedLocations);
-	} else {
+	}
+
+	function sortLocationsByType() {
+		const sortByTypeOrdering = ["pipe", "hidden"];
+
 		let sortedLocations = document.createDocumentFragment();
 
-		typeOrdering.forEach(function (typeName) {
+		sortByTypeOrdering.forEach(function (typeName) {
 			let locationsOfTypeNodeList = document.querySelectorAll(
 				`.location[data-type=${typeName}]`,
 			);
@@ -74,10 +97,18 @@ toggleChronologicalButton.addEventListener("click", function () {
 			});
 		});
 
-		let locationWrapper = document.querySelector("#location-wrapper");
 		locationWrapper.innerHtml = null;
 		locationWrapper.appendChild(sortedLocations);
 	}
-});
+}
+
 // type: location.dataset.type,
 // nextLocation: location.dataset.chronologicalNextLocation,
+
+// TODO: We'll need a mapping of location types to title text
+// TODO: The location type ordering array will need to be expanded
+// TODO: The code needs to be made generic, only data should need to be edited to support more location types
+//
+
+// Idea: can we store the state on page load and consider that the sort by type, and then after we do the chronological sort once cache the edited sort?
+// That would simplify the code and make it faster
