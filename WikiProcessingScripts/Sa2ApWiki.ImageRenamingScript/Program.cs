0ca145adfcdb1e500ac0294f5f9e0ecaa5bc3032
JoshuaEagles <joshuaeagles1@gmail.com>
2025-04-26 22:40:35 -0400

    rename screenshots to include the stage name in the name

--- WikiProcessingScripts/Sa2ApWiki.ImageRenamingScript/Program.cs
+++ WikiProcessingScripts/Sa2ApWiki.ImageRenamingScript/Program.cs
@@ -40,68 +40,19 @@ return;
 
 void PerformFileRename(string stageName, string directoryPath)
 {
-	var relevantChronologicalLocationsForStage = chronologicalLocations[stageName]
-		.Where(x => x.LocationName.Contains("pipe") || x.LocationName.Contains("hidden") || x.LocationName.Contains("animal"))
-		.ToImmutableArray();
-
-	var locationPaths = Directory.EnumerateFiles(directoryPath, "*.webp", SearchOption.AllDirectories);
+	var locationScreenshots = Directory.EnumerateFiles(directoryPath, "*.webp", SearchOption.AllDirectories)
+		.Concat(Directory.EnumerateFiles(directoryPath, $"*.png", SearchOption.TopDirectoryOnly))
+		.Select(screenshotPath => new LocationScreenshot(screenshotPath));
 	
-	var pngScreenshots = Directory.EnumerateFiles(directoryPath, $"*.png", SearchOption.TopDirectoryOnly);
-	var webpScreenshots = Directory.EnumerateFiles(directoryPath, $"*.webp", SearchOption.TopDirectoryOnly);
-	var locationsScreenshotsLookup = pngScreenshots.Concat(webpScreenshots)
-		.Select(screenshotPath => new LocationScreenshot(screenshotPath))
-		.ToLookup(x => x.LocationName);
-	
-	foreach (var locationScreenshotGroup in locationsScreenshotsLookup)
+	foreach (var locationScreenshot in locationScreenshots)
 	{
-		foreach (var locationScreenshot in locationScreenshotGroup)
-		{
-			var filePath = $"{directoryPath}/{locationScreenshot.FileName}";
-			if (locationScreenshot.LocationType == "animal" && locationScreenshot.IsBonus == false)
-			{
-				var chronologicalIndexOfAnimal =
-					FindIndex(relevantChronologicalLocationsForStage, locationScreenshot.LocationName);
-
-				if (chronologicalIndexOfAnimal == 0)
-				{
-					continue;
-				}
-				
-				var chronologicalLocationBeforeChronologicalAnimal = relevantChronologicalLocationsForStage[chronologicalIndexOfAnimal - 1];
-
-				if (!chronologicalLocationBeforeChronologicalAnimal.LocationName.Contains("pipe") &&
-				    !chronologicalLocationBeforeChronologicalAnimal.LocationName.Contains("hidden"))
-				{
-					continue;
-				}
-				
-				var screenshotSplit = chronologicalLocationBeforeChronologicalAnimal.LocationName.Split('-');
-				var linkedLocationType = screenshotSplit[0];
-				var linkedLocationNumber = screenshotSplit[1];
-				
-				var newFileName =
-					$"{linkedLocationType}-{linkedLocationNumber}-{locationScreenshot.ScreenshotNumber}.webp";
-			
-				Console.WriteLine(
-					$"Copying {filePath} to {directoryPath}/{newFileName}");
-				File.Copy(filePath, $"{directoryPath}/{newFileName}");
-			}
-		}
-
-
-		// File.Move($"{directoryPath}/{locationScreenshot.FileName}", $"{directoryPath}/{newFileName}");
+		var filePath = $"{directoryPath}/{locationScreenshot.FileName}";
+		
+		var newFileName =
+			$"{stageName}-{locationScreenshot.LocationType}-{(locationScreenshot.IsBonus ? "bonus" : "")}{locationScreenshot.LocationNumber}-{locationScreenshot.ScreenshotNumber}{Path.GetExtension(filePath)}";
+		
+		Console.WriteLine(
+			$"Renaming {filePath} to {directoryPath}/{newFileName}");
+		File.Move(filePath, $"{directoryPath}/{newFileName}");
 	}
-}
-
-int FindIndex(ImmutableArray<ChronologicalLocationModel> chronologicalLocationsForStage, string locationName)
-{
-	foreach (var chronologicalLocation in chronologicalLocationsForStage)
-	{
-		if (chronologicalLocation.LocationName == locationName)
-		{
-			return chronologicalLocationsForStage.IndexOf(chronologicalLocation);
-		}
-	}
-	
-	throw new ArgumentException("Not found");
 }
