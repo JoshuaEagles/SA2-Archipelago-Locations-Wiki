35d0195fcc3280c94a0e35199b22e329ddc33efd
JoshuaEagles <joshuaeagles1@gmail.com>
2025-05-10 21:00:53 -0400

    update ImageRenamingScript to zero pad location numbers

--- WikiProcessingScripts/Sa2ApWiki.ImageRenamingScript/Program.cs
+++ WikiProcessingScripts/Sa2ApWiki.ImageRenamingScript/Program.cs
@@ -1,28 +1,10 @@
-using System.Collections.Immutable;
 using Sa2ApWiki.Common;
 using Sa2ApWiki.Common.Models;
-using Sa2ApWiki.Common.Services;
 
-var path = "/home/entiss/Projects/SA2-Archipelago-Locations-Wiki/";
-
-var chronologicalLocationDocumentPath = Path.Join(path, "ChronologicalLocationOrder.txt");
-var chronologicalLocations = ChronologicalLocationOrderParser.Parse(chronologicalLocationDocumentPath);
+const string path = "/home/entiss/Projects/SA2-Archipelago-Locations-Wiki/";
 
 foreach (var characterName in Constants.CharacterNames)
 {
-	// Go through each directory in the character folders
-	// For each one not named Chronological:
-	// Generate a file based on the Folder name, <Name>Chronological.md
-        
-	// In the file: 
-            
-	// ## <Name> Item n
-	// ![](<path-to-screenshot>)
-	// repeat for each one
-	// Empty line
-        
-	// Same idea for Lives
-        
 	foreach (var directoryPath in Directory.EnumerateDirectories(Path.Join(path, characterName), string.Empty, SearchOption.TopDirectoryOnly))
 	{
 		if (directoryPath.Contains("Chronological"))
@@ -48,11 +30,18 @@ void PerformFileRename(string stageName, string directoryPath)
 	{
 		var filePath = $"{directoryPath}/{locationScreenshot.FileName}";
 		
+		var zeroPaddedLocationNumber = ZeroPadNumber(locationScreenshot.LocationNumber, 2);
+		
 		var newFileName =
-			$"{stageName}-{locationScreenshot.LocationType}-{(locationScreenshot.IsBonus ? "bonus" : "")}{locationScreenshot.LocationNumber}-{locationScreenshot.ScreenshotNumber}{Path.GetExtension(filePath)}";
+			$"{stageName}-{locationScreenshot.LocationType}-{(locationScreenshot.IsBonus ? "bonus" : "")}{zeroPaddedLocationNumber}-{locationScreenshot.ScreenshotNumber}{Path.GetExtension(filePath)}";
 		
 		Console.WriteLine(
 			$"Renaming {filePath} to {directoryPath}/{newFileName}");
 		File.Move(filePath, $"{directoryPath}/{newFileName}");
 	}
+}
+
+string ZeroPadNumber(int number, int length)
+{
+	return number.ToString().PadLeft(length, '0');
 }
